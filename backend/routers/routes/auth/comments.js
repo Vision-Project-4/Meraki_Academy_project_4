6215bfcabf291718c9f201e95eb53c072c45f968
AsmaRalenazi <AsmaAlenazi13@gmail.com>
2021-06-03 10:54:18 +0300

    done Create New Comment _Asma

---
+++ backend/routers/routes/auth/comments.js
@@ -0,0 +1,18 @@
+
+// Create new comments -Asma
+app.post("/articles/:id/comments",(req,res)=>{
+    let {comment,commenter}=req.body
+    const id=req.params.id
+    const Comment1=new comment({
+        comment,
+        commenter,
+    })
+    
+    Comment1
+    .save()
+    .then((result)=>{
+res.json(result)
+    }).catch((err)=>{
+        res.status(201)
+    })
+})
